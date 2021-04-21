a79830abc0b849ec94732a161b367c0b01f49462
Antonio_Aranda7 <jeancarloantonioaranda@gmail.com>
2021-04-20 20:47:50 -0500

    create app

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app';
+  numeroClics= 10;
+  
+  materias = ["Desarrollo web II", "Framework Frontend", "Redes"]
+}
+/*
+export 
+
+  art={
+    codigo:null,
+    description:null,
+    precio:null
+  }
+
+  agregar(){
+    for(let x=0;x<this.articulos.length;x++)
+    if
+  }
+//Comom definein un objeto apra una interface
+*/
